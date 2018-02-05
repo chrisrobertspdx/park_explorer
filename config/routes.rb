df79fe01b39e0ebe353d8f89f302c461c7a50979
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-05 16:43:10 +0000

    nested resource photo in parks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   root to: "parks#index"
   
   resources :categories
-  resources :photos
-  resources :parks
+  #resources :photos
+  #resources :parks
+  
+  resources :parks do
+    resources :photos
+  end
   
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
