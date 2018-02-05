5290504120a95ddada39133bf1959cfa90db117a
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-05 20:05:31 +0000

    added custom attribute writer method to Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,18 @@ class Photo < ApplicationRecord
     has_many :photo_categories
     has_many :categories, :through => :photo_categories
     
+    #attr_accessor :categories
     
+    def categories_attributes=(data)
+        #binding.pry
+        #need to validate
+        newCat = data["0"][:name].downcase.capitalize
+        
+        if newCat != ""
+            @category = Category.find_or_create_by(name: newCat)
+            self.categories << @category
+        end
+        
+    end
     
 end
