dc4b7bf6581c067dc6cb615052960652c869aa57
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-05 20:13:29 +0000

    added carrierwave uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205174509) do
+ActiveRecord::Schema.define(version: 20180205201224) do
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20180205174509) do
     t.datetime "updated_at", null: false
     t.integer "park_id"
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
